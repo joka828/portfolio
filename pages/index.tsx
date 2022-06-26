78d21127fefac81ead7197332b54d46fe1b55dbd
Joka Canda <joaquin.candalaft@gmail.com>
2022-06-26 17:00:18 -0300

    add meta preview image

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const Dashboard: NextPage = () => {
         <meta name="description" content="Joaquin Candalaft&apos;s portfolio" />
         <meta name="keywords" content="Joaquin, Candalaft, portfolio, linkedin, developer" />
         <meta name="author" content="Joaquin Candalaft" />
+        <meta property="og:image" content="https://joaquincandalaft.com/avatar.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
