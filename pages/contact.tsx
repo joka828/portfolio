b1dd9de679043aec2f2a063d1eee626667fb4ebb
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-05-10 13:57:39 -0300

    add contact view meta tags

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,43 +3,49 @@ import ContentCopyOutlinedIcon from "@mui/icons-material/ContentCopyOutlined";
 
 import { copyEmailTracking, downloadCVTracking } from "../helpers/trackings";
 import styles from "../styles/Contact.module.css";
-import { Tooltip } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
 import toast from "react-hot-toast";
+import Head from "next/head";
 
 const email = "joaquin.candalaft@gmail.com";
 
 const Contact: NextPage = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.section}>
-        <span className={styles.contactText}>
-          Reach to me at
-          <Tooltip title="Copy email">
-            <b
-              className={styles.emailText}
-              onClick={() => {
-                navigator.clipboard.writeText(email);
-                copyEmailTracking();
-                toast.success("Email copied to clipboard!");
-              }}
-            >
-              <ContentCopyOutlinedIcon /> {email}
-            </b>
-          </Tooltip>
-        </span>
-      </div>
-      <div className={styles.section}>
-        You can also{" "}
-        <a
-          href="/resume.pdf"
-          target="_blank"
-          rel="noreferrer"
-          onClick={downloadCVTracking}
-        >
-          <b className={styles.resumeLink}>download my resume!</b>
-        </a>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Contact Joaquin Candalaft!</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.section}>
+          <span className={styles.contactText}>
+            Reach to me at
+            <Tooltip title="Copy email">
+              <b
+                className={styles.emailText}
+                onClick={() => {
+                  navigator.clipboard.writeText(email);
+                  copyEmailTracking();
+                  toast.success("Email copied to clipboard!");
+                }}
+              >
+                <ContentCopyOutlinedIcon /> {email}
+              </b>
+            </Tooltip>
+          </span>
+        </div>
+        <div className={styles.section}>
+          You can also{" "}
+          <a
+            href="/resume.pdf"
+            target="_blank"
+            rel="noreferrer"
+            onClick={downloadCVTracking}
+          >
+            <b className={styles.resumeLink}>download my resume!</b>
+          </a>
+        </div>
+      </main>
+    </>
   );
 };
 
