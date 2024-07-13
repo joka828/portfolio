320ba352eb5b315080f7e6738598fffd20c5aae3
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-07-12 22:54:00 -0300

    fix pdf link

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/resume.pdf", request.url));
+  return NextResponse.redirect(
+    new URL("/resume-joaquin-candalaft.pdf", request.url)
+  );
 }
 
 export const config = {
--- pages/projects/resume.tsx
+++ pages/projects/resume.tsx
@@ -2,10 +2,6 @@ import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 const Resume = () => {
-  useEffect(() => {
-    redirect("/resume-joaquin-candalaft.pdf");
-  }, []);
-
   return (
     <div>
       <h1>Resume</h1>
